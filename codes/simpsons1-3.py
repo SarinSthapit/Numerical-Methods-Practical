664b32dad5157730c2adeb051335e507109f7a83
SarinSthapit <sarinsthapit.2001@gmail.com>
2022-05-12 18:58:11 +0545

    Trapezoidal Rule and Both Simpson's Rules completed

--- codes/simpsons1-3.py
+++ codes/simpsons1-3.py
@@ -1,6 +1,6 @@
 from prettytable import PrettyTable
 
-def trapezoidal(initial_x, final_x, n, accuracy):
+def simpsons1_3(initial_x, final_x, n, accuracy):
     def f(x):
         f = x**2 + 2.71828**x
         return f
@@ -18,14 +18,14 @@ def trapezoidal(initial_x, final_x, n, accuracy):
         if(i == 0 or i == 6):
             sum = round(sum + y, accuracy + 1)
 
-        elif(i<6 and i>0):
+        elif(i % 2 == 0):
             sum = round(sum + (2*y), accuracy + 1)
 
         else:
-            print("It is out of range.")
+            sum = round(sum + (4*y), accuracy + 1)
 
 
-    solution = round((h/2) * sum, accuracy + 1)
+    solution = round((h/3) * sum, accuracy + 1)
     print(f"The required solution is {solution}.")
 
 
@@ -34,4 +34,7 @@ final_x = float(input("Enter the final value of x: "))
 n = int(input("Enter the number of subintervals: "))
 accuracy = int(input("Enter the accuracy of the decimal digits: ")) 
 
-trapezoidal(initial_x, final_x, n, accuracy)
+if(n%2 == 0):
+    simpsons1_3(initial_x, final_x, n, accuracy)
+else:
+    print("The number of subintervals in Simpson's 1/3 Rule must be even.")
--- codes/simpsons3-8.py
+++ codes/simpsons3-8.py
@@ -1,6 +1,6 @@
 from prettytable import PrettyTable
 
-def trapezoidal(initial_x, final_x, n, accuracy):
+def simpsons3_8(initial_x, final_x, n, accuracy):
     def f(x):
         f = x**2 + 2.71828**x
         return f
@@ -18,14 +18,14 @@ def trapezoidal(initial_x, final_x, n, accuracy):
         if(i == 0 or i == 6):
             sum = round(sum + y, accuracy + 1)
 
-        elif(i<6 and i>0):
+        elif(i % 3 == 0):
             sum = round(sum + (2*y), accuracy + 1)
 
         else:
-            print("It is out of range.")
+            sum = round(sum + (3*y), accuracy + 1)
 
 
-    solution = round((h/2) * sum, accuracy + 1)
+    solution = round((3*h/8) * sum, accuracy + 1)
     print(f"The required solution is {solution}.")
 
 
@@ -34,4 +34,7 @@ final_x = float(input("Enter the final value of x: "))
 n = int(input("Enter the number of subintervals: "))
 accuracy = int(input("Enter the accuracy of the decimal digits: ")) 
 
-trapezoidal(initial_x, final_x, n, accuracy)
+if(n%2 == 0):
+    simpsons3_8(initial_x, final_x, n, accuracy)
+else:
+    print("The number of subintervals in Simpson's 1/3 Rule must be even.")
